5e9582ec65a107ca13b07075a63ed12c58ee63a7
Ethan Lynn <ethanlynn0709@gmail.com>
2023-07-18 02:43:42 -0400

    feat: boilerplate for setting up notify-rs and REPL at same time

--- src/vamp/src/main.rs
+++ src/vamp/src/main.rs
@@ -1,18 +1,44 @@
+use notify::RecursiveMode;
+use notify_debouncer_mini::{new_debouncer, DebounceEventResult};
 use rustyline::error::ReadlineError;
-use rustyline::{DefaultEditor, Result};
+use rustyline::DefaultEditor;
+use std::{path::Path, time::Duration};
 use vamp_eval::{eval_stmt, Scope};
 use vamp_sym::Interner;
 use vamp_syntax::parser::parse_stmt;
 
-fn main() -> Result<()> {
-    let mut editor = DefaultEditor::new()?;
+fn reload_file(path: &Path) {}
+
+fn main() {
+    let root_path = Path::new(".");
+    let mut debouncer = new_debouncer(
+        Duration::from_secs(1),
+        None,
+        |event: DebounceEventResult| match event {
+            Ok(events) => {
+                for event in events {
+                    if event.path.extension() == Some("vamp".as_ref()) {
+                        reload_file(&event.path);
+                    }
+                }
+            }
+            Err(errors) => {
+                eprintln!("error: {:?}", errors);
+            }
+        },
+    )
+    .unwrap();
+    let watcher = debouncer.watcher();
+    watcher.watch(root_path, RecursiveMode::Recursive).unwrap();
+
+    let mut editor = DefaultEditor::new().unwrap();
     let mut interner = Interner::new();
     let mut scope = Scope::default();
     loop {
         let readline = editor.readline("> ");
         match readline {
             Ok(line) => {
-                editor.add_history_entry(&line)?;
+                editor.add_history_entry(&line).unwrap();
                 match parse_stmt(&line, &mut interner) {
                     Err(error) => {
                         eprintln!("error: {:?}", error);
@@ -27,10 +53,18 @@ fn main() -> Result<()> {
                     },
                 }
             }
-            Err(ReadlineError::Interrupted) => break,
-            Err(ReadlineError::Eof) => break,
-            Err(_) => break,
+            Err(ReadlineError::Interrupted) => {
+                println!("Ctrl-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Ctrl-D");
+                break;
+            }
+            Err(error) => {
+                eprintln!("error: {:?}", error);
+                break;
+            }
         }
     }
-    Ok(())
 }
